6bd3db5009000a72d3265cba4f77824678c11d89
Jasmine <jasminecharles@hotmail.com>
2019-11-20 11:48:43 -0800

    userType

--- users/users-model.js
+++ users/users-model.js
@@ -37,7 +37,7 @@ return db('users');
 
  function find() {
   return db('users') 
-  .select('id', 'username', 'location',)  
+  .select('id', 'username', 'location','userType')  
  }
 
 function findBy(filter) {
